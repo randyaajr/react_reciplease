9136e0c7bb778df33aa9c8bc1aa58a7f1308e7d2
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-03-18 20:18:55 -0500

    Prop fetches title calories and image from api data

--- src/App.js
+++ src/App.js
@@ -31,7 +31,11 @@ const App = () => {
         </button>
       </form>
       {recipes.map((recipe) => (
-        <Recipe />
+        <Recipe
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
       ))}
     </div>
   );
