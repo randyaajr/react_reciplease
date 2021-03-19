d0f9b68a36612735c4d1ffd9985fd17bec851281
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-03-18 19:53:07 -0500

    Import useEffect and useState. API basic logic created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
   const APP_ID = "53e91caa";
   const APP_KEY = "172777cd621aecb4357bd994353f4b78";
 
-  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    // eslint-disable-next-line no-unused-expressions
+    console.log(data);
+  };
 
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
         <button className="search-button" type="submit">
-          Search here
+          Search
         </button>
       </form>
     </div>
