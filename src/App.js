75261d5cfdd7d3b9b42663bf2b8c57f8dccf1874
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-03-18 19:12:02 -0500

    Added API ID and KEY along with the request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "53e91caa";
+  const APP_KEY = "172777cd621aecb4357bd994353f4b78";
+
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React</h1>
     </div>
   );
-}
+};
 
 export default App;
